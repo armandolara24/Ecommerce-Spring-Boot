e51a320611ff3758ca88fac0b71a289302e93517
armandolara24 <armandolaramail@gmail.com>
2023-11-12 18:40:37 -0600

    add Stripe support

--- src/main/java/com/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/ecommerce/service/CheckoutServiceImpl.java
@@ -1,24 +1,32 @@
 package com.ecommerce.service;
 
 import com.ecommerce.dao.CustomerRepository;
+import com.ecommerce.dto.PaymentInfo;
 import com.ecommerce.dto.Purchase;
 import com.ecommerce.dto.PurchaseResponse;
 import com.ecommerce.entity.Customer;
 import com.ecommerce.entity.Order;
 import com.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
     private CustomerRepository customerRepository;
 
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key.secret}") String secretKey){
         this.customerRepository = customerRepository;
+
+        // initialize Stripe API with secret key
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -34,7 +42,7 @@ public class CheckoutServiceImpl implements CheckoutService {
 
         // populate order with orderItems
         Set<OrderItem> orderItems = purchase.getOrderItems();
-        orderItems.forEach(item -> order.add(item));
+        orderItems.forEach(order::add);
 
         // populate order with billingAddress and shippingAddress
         order.setBillingAddress(purchase.getBillingAddress());
@@ -60,6 +68,19 @@ public class CheckoutServiceImpl implements CheckoutService {
 
         // return a response
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
